54609ce2b106a906e8f5640bc48bed3fc037a6de
GoldAbsolute <sokoldima1998@mail.ru>
2022-08-26 17:29:02 +0300

    go 04

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
 func check(err error) {
@@ -13,11 +15,24 @@ func check(err error) {
 	}
 }
 
+var APP_LOGIN = os.Getenv("app_login")
+var APP_PASSWORD = os.Getenv("app_password")
+var APP_IP = os.Getenv("app_ip")
+var APP_PORT = os.Getenv("app_port")
+var APP_DBNAME = os.Getenv("app_dbname")
+
+var db_path = fmt.Sprintf("%s:%s@(%s:%s)/%s", APP_LOGIN, APP_PASSWORD, APP_IP, APP_PORT, APP_DBNAME)
+
 func main() {
+	fmt.Printf("appLogin: %s\nappPassword: %s\nappIp: %s\nappPort: %s\nappDBname: %s\n", APP_LOGIN, APP_PASSWORD, APP_IP, APP_PORT, APP_DBNAME)
+	db, err := sql.Open("mysql", db_path)
+	check(err)
+	check(db.Ping())
+	fmt.Println("DataBase ping is OK")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := fmt.Fprintf(writer, "string: %s", "123")
+		_, err := fmt.Fprintf(writer, "string: %s", db_path)
 		check(err)
 	})
 	errHttp := http.ListenAndServe(":80", router)
